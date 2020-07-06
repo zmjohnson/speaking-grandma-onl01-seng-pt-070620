363bd536248d04f77e5c103ed89500f822c59808
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-06 19:01:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
-  elsif speak_to_grandma = ('I LOVE YOU GRANDMA!')
-    return 'I LOVE YOU TOO PUMPKIN!'
+  else
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end 
